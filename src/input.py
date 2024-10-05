e888e5bd4ede51ab029d479a7371433f4765bf05
drago-suzuki58 <dorasuzu58@gmail.com>
2024-10-05 15:31:47 +0900

    feat: stringsステートメントを独立した辞書に収納するように変更

--- src/input.py
+++ src/input.py
@@ -15,6 +15,8 @@ class DialogueTab:
     def __init__(self):
         self.original_dialogues = {}
         self.translated_dialogues = {}
+        self.original_strings = {}
+        self.translated_strings = {}
     
     def load(self, original_filename, translated_filename):
         self.load_original(original_filename)
@@ -26,13 +28,15 @@ class DialogueTab:
             next(reader)
             counter = 1
             for line_number, row in enumerate(reader):
+                # ファイルの形式が正しくなく、最後のtabが抜けているときには空文字を最後に追加
+                if len(row) < 6:
+                    row.append("")
                 # stringsステートメントはIDが空なので上から順番になるように一旦仮IDを振る(処理は今度実装)
                 if row[0] == "":
                     row[0] = f"strings_{counter}"
+                    self.original_strings[row[0]] = DialogueLine(row[0], row[1], row[2], row[3], int(row[4]), row[5])
                     counter += 1
-                # ファイルの形式が正しくなく、最後のtabが抜けているときには空文字を最後に追加
-                if len(row) <= 6:
-                    row.append("")
+                    continue
                 self.original_dialogues[row[0]] = DialogueLine(row[0], row[1], row[2], row[3], int(row[4]), row[5])
     
     def load_translated(self, filename):
@@ -41,11 +45,13 @@ class DialogueTab:
             next(reader)
             counter = 1
             for line_number, row in enumerate(reader):
+                # ファイルの形式が正しくなく、最後のtabが抜けているときには空文字を最後に追加
+                if len(row) < 6:
+                    row.append("")
                 # stringsステートメントはIDが空なので上から順番になるように一旦仮IDを振る(処理は今度実装)
                 if row[0] == "":
                     row[0] = f"strings_{counter}"
+                    self.translated_strings[row[0]] = DialogueLine(row[0], row[1], row[2], row[3], int(row[4]), row[5])
                     counter += 1
-                # ファイルの形式が正しくなく、最後のtabが抜けているときには空文字を最後に追加
-                if len(row) <= 6:
-                    row.append("")
+                    continue
                 self.translated_dialogues[row[0]] = DialogueLine(row[0], row[1], row[2], row[3], int(row[4]), row[5])
